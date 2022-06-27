760ad72520fb8830fb0cda70edeabc9044c79295
Anton Panteleev <panteleev23@gmail.com>
2022-06-27 22:17:03 +1000

    Delete _site junk

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "G'Day! I'm Anton 🤠",
-        "excerpt":"Fullstack software engineer, based in Melbourne, Australia. I switched into tech after more than a decade in Civil Engineering and Project Management consulting - I rediscovered my passion and living the dream now, building digital products and hoping to make the world a better place. I am focused on back-end,...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/welcome/",
-        "teaser": null
-      }]
